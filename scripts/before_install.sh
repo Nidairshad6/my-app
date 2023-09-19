96514e222457d55721637cabae5de5fdf964bb17
Nidairshad6 <141719167+Nidairshad6@users.noreply.github.com>
2023-09-19 12:40:38 +0500

    change

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,6 @@
 set -e
 
 
-# Authenticate with ECR (Assuming your AWS credentials are already configured on the instance)
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 875681151031.dkr.ecr.us-east-1.amazonaws.com
+
 
 docker pull 875681151031.dkr.ecr.us-east-1.amazonaws.com/first-repo:latest
